e6be94ff2c5d897d495b9477a4599760a5a5672c
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2025-04-03 13:13:57 -0700

    Update boot.lua

--- etc/boot.lua
+++ etc/boot.lua
@@ -56,9 +56,9 @@
   LNG_TESTSUCC = "Test succeeded!"
   LNG_INSTALLING = "Installing System Updates..."
   LNG_INSTFINISH = "Installation finished!"
-  LNG_WANNAQUIT = "Add an OpenTuna Icon Exploit to launch mc?/BOOT/BOOT.ELF from the PS2 Browser,\niideal entry for consoles which can not auto boot exploits.\n(Make sure you have installed at least 1 form of UMCS before installing OpenTuna. This ensures the presence of required\nmc?:/BOOT/BOOT.ELF)"
+  LNG_WANNAQUIT = "Add an OpenTuna Icon Exploit to launch mc?/BOOT/BOOT.ELF from the PS2 Browser,\nallowing for entry on any consoles which can not auto boot exploits.\n(Make sure you have installed at least 1 form of UMCS before installing OpenTuna.\nThis ensures the presence of required mc?:/BOOT/BOOT.ELF)"
   LNG_YES = "Yes"
-  LNG_YESALT = "Exit and Launch OpenTuna Installer for mc?:/BOOT/BOOT.ELF"
+  LNG_YESALT = "Exit and Launch OpenTuna Icon Installer
   LNG_NO = "No"
   LNG_NOALT = "Go Back to KELFBinder Menu"
   LNG_PLS_WAIT = "Please Wait..."
